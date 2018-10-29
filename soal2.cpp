a3ce5da108b8fe8c8c485831ac62a83af2cb1d16
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-10-29 10:38:49 +0700

    aplot soal2

---
+++ soal2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int N, X, T, Batas;
+
+    cout << "masukan nilai n";
+    cin>> N;
+
+    Batas = N + 100;
+    X = 20;
+    T = N;
+
+    while ( T <= Batas ) {
+        T= T+X;
+        X=X+10;
+    }
+
+cout<< T;
+
+}
